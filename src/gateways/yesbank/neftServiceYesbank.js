37f51b1dd2331840754bb47e100a1ac0ed3e01af
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-05-08 15:35:08 +0530

    updates

--- src/gateways/yesbank/neftServiceYesbank.js
+++ src/gateways/yesbank/neftServiceYesbank.js
@@ -1,4 +1,4 @@
- export default async function neftServiceYesbank(){
+  async function neftServiceYesbank(){
 
     const requestBody = {
       "transfer": {
@@ -63,6 +63,72 @@
       return response
 }
 
-// export default  {
-//   neftServiceYesbank
-//   };
+async function neftServiceYesbank2(){
+
+  const requestBody = {
+    "transfer": {
+      "version": "1",
+      "uniqueRequestNo": "7866646464",
+      "appID": "265398",
+      "customerID": "265765",
+      "debitAccountNo": "000183200000042",
+      "beneficiary": {
+        "beneficiaryDetail": {
+          "beneficiaryName": {
+            "fullName": "AJAY RANA"
+          },
+          "beneficiaryAddress": {
+            "address1": "Willston Road",
+            "postalCode": "123659",
+            "city": "Mumbai",
+            "country": "IN"
+          },
+          "beneficiaryContact": {
+            "mobileNo": "9561234523",
+            "emailID": "Gourav Darjee@qualitykiosk.com"
+          },
+          "beneficiaryAccountNo": "50100002965304",
+          "beneficiaryIFSC": "HDFC0001626",
+          "beneficiaryMobileNo": "9869581569",
+          "beneficiaryMMID": "9532870"
+        }
+      },
+      "transferType": "NEFT",
+      "transferCurrencyCode": "INR",
+      "transferAmount": "20",
+      "remitterToBeneficiaryInfo": "FUND TRANSFER"
+    }
+  };
+  
+  const response = await fetch('xyz', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "X-IBM-Client-Id":"",
+      "X-IBM-Client Secret":""
+    },
+    body: JSON.stringify(requestBody)
+  })
+    .then(response => {
+      console.log(response)
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Success:', data);
+      // Handle the response data here
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Handle errors here
+    });
+  
+    return response
+}
+
+export default  {
+  neftServiceYesbank,
+  neftServiceYesbank2
+  };
--- src/neft/neftServices.js
+++ src/neft/neftServices.js
@@ -7,7 +7,7 @@ import neftServiceYesbank from "../gateways/yesbank/neftServiceYesbank.js";
 
 async function neftTransfer(){
 
-      const response = await neftServiceYesbank.neftTransfer()
+      const response = await neftServiceYesbank.neftServiceYesbank2()
       return response
 }
 
