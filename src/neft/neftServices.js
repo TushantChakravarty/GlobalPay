806e23b81adb691d26429c3585e50d4e0ad202c7
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-05-13 15:34:50 +0530

    Update neftServices.js

--- src/neft/neftServices.js
+++ src/neft/neftServices.js
@@ -1,14 +1,6 @@
 import { neftYesbank } from "../gateways/yesbank/neftServiceYesbank.js";
 
-
-
-
-
-
-
-export async function neftTransfer(){
-
-      const response = await neftYesbank()
-      return response
+export async function neftTransfer() {
+  const response = await neftYesbank();
+  return response;
 }
-
