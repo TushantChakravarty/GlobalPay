4f399c4e1be8926ac56eb0ed303ff60d250e0d00
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-05-15 16:24:18 +0530

    updates

--- src/payin/upi/payinRoutes.js
+++ src/payin/upi/payinRoutes.js
@@ -1,14 +1,20 @@
 import { bulkpeUpiCollect } from "../../gateways/bulkpe/bulkpe.js";
 
+async function payinUpiRoutes(fastify, options) {
+    fastify.post('/upiCollect', {
+      schema: {
+        body: {
+            type: 'object',
+            required: ['amount'],
+            properties: {
+                amount: { type: 'number', minimum: 0.01 },
+            }
+        }
+    }
+    }, async (request, reply) => {
+        const response = await bulkpeUpiCollect();
+        return reply.send(response);
+    });
+}
 
-async function payinUpiRoutes (fastify, options) {
-
-    fastify.get('/upiCollect', async (request, reply) => {
-      const response =await bulkpeUpiCollect();
-      return reply.send(response) 
-    })
-
-
-  }
-  
 export default payinUpiRoutes;
