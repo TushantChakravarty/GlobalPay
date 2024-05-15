9e25be73e508d11b836a36ed0b01f8a3680afa30
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-05-15 16:26:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Fastify from 'fastify'
 import registerRoutes from './src/routes/index.js'
-import { createPayout, createPayoutVpa } from './src/gateways/razorpay/razorpayService.js';
-import { bulkpeUpiCollect } from './src/gateways/bulkpe/bulkpe.js';
-import { cashfreePayin } from './src/gateways/cashfree/cashfree.js';
 const fastify = Fastify({
   logger: true
 })
-//bulkpeUpiCollect()
-// cashfreePayin({
-//     amount:10,
-//     phone:9340079982
-// })
+
 const startServer = async ()=>{
 
     try {
@@ -24,5 +17,4 @@ const startServer = async ()=>{
     }
 }
 
-// Run the server!
 startServer()
