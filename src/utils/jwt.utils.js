4e7ed65b6f9046f302b79b0ae01eb62db40285a2
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-05-17 13:05:17 +0530

    updates

--- src/utils/jwt.utils.js
+++ src/utils/jwt.utils.js
@@ -1,25 +1,21 @@
 import { validateApiKey } from "./password.utils.js";
 
-export async function generateUserToken(details,fastify)
-{
-    const { email_id} = details
-    const token = fastify.jwt.sign({ email_id });
-    return token 
+export async function generateUserToken(details, fastify) {
+  const { email_id } = details;
+  const token = fastify.jwt.sign({ email_id });
+  return token;
 }
 
 export const validateToken = async (request, reply) => {
-    try {
-     const validated =  await request.jwtVerify();
-     console.log('validated',validated)
-     return validated
-    } catch (err) {
-        console.log(err)
-      reply.status(401).send({message:'invalid token'});
-    }
-  };
+  try {
+    const validated = await request.jwtVerify();
+  } catch (err) {
+    console.log(err);
+    reply.status(401).send({ message: "invalid token" });
+  }
+};
 
-  export const validateTokenAndApiKey = async (request, reply) => {
-    await validateToken(request, reply);
-   await validateApiKey(request, reply);
-   
-  };
+export const validateTokenAndApiKey = async (request, reply) => {
+  await validateToken(request, reply);
+  await validateApiKey(request, reply);
+};
--- src/utils/password.utils.js
+++ src/utils/password.utils.js
@@ -1,60 +1,58 @@
-import bcrypt from "bcryptjs"
-import CryptoJS from 'crypto-js';
+import bcrypt from "bcryptjs";
+import CryptoJS from "crypto-js";
 import { findUser } from "../user/userDao.js";
 export function generatePassword(len, arr) {
-  let ans = '';
+  let ans = "";
   for (let i = len; i > 0; i--) {
-    ans +=
-      arr[(Math.floor(Math.random() * arr.length))];
+    ans += arr[Math.floor(Math.random() * arr.length)];
   }
   console.log(ans);
-  return ans
+  return ans;
 }
 
 export async function convertPass(password) {
-  let pass = await bcrypt.hash(password, 10)
+  let pass = await bcrypt.hash(password, 10);
   // req.body.password = pass;
-  return pass
+  return pass;
 }
 
 export function encryptText(input) {
-  var ciphertext = CryptoJS.AES.encrypt(input, process.env.SECRETKEY).toString();
-  console.log(ciphertext)
-  return ciphertext
-
+  var ciphertext = CryptoJS.AES.encrypt(
+    input,
+    process.env.SECRETKEY
+  ).toString();
+  console.log(ciphertext);
+  return ciphertext;
 }
 
 export const validateApiKey = async (request, reply) => {
-  const apiKey = request.headers['apikey'];
-  console.log('apiKey',apiKey)
+  const apiKey = request.headers["apikey"];
+  console.log("apiKey", apiKey);
   if (!apiKey) {
-    return reply.status(401).send({ message: 'Missing API key' });
+    return reply.status(401).send({ message: "Missing API key" });
   }
 
   try {
     var bytes = CryptoJS.AES.decrypt(apiKey, process.env.SECRETKEY);
     var originalText = bytes.toString(CryptoJS.enc.Utf8);
-    console.log(originalText)
+    console.log(originalText);
     if (!originalText) {
-      return reply.status(401).send({ message: 'Invalid API key' });
+      return reply.status(401).send({ message: "Invalid API key" });
     }
-    const user = await findUser(request?.body?.email_id)
-    console.log(user)
-    if(!user)
-    {
-      return reply.status(401).send({ message: 'Invalid User' });
+    const user = await findUser(request?.body?.email_id);
+    console.log(user);
+    if (!user) {
+      return reply.status(401).send({ message: "Invalid User" });
     }
-    if(user?.apiKey == originalText)
-    {
+    if (user?.apiKey == originalText) {
       //console.log(user?.apiKey == originalText)
       request.apiKeyDetails = originalText;
-    }else{
-      return reply.status(401).send({ message: 'Invalid API key' });
-
+    } else {
+      return reply.status(401).send({ message: "Invalid API key" });
     }
     // You can perform additional checks here if needed
     // Storing the decrypted details in request for further use
   } catch (error) {
-    return reply.status(401).send({ message: 'Invalid API key' });
+    return reply.status(401).send({ message: "Invalid API key" });
   }
 };
