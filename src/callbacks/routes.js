55c79047ba58d4661946d08d8957737514b0f1cb
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-06-09 11:50:59 +0530

    Update routes.js

--- src/callbacks/routes.js
+++ src/callbacks/routes.js
@@ -30,7 +30,7 @@ async function callbackRoutes(fastify, options) {
     let details = request?.body
      //console.log('detailssssssss payout',details?.payload?.payout?.entity)
      console.log(details)
-     let data =details?.payload?.payout?.entity
+     let data =details
     if (
       data?.status=='processed'||data?.status=="failed"
     ) {
