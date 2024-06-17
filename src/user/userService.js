7338f2b83e0f8386c6641b3b65c6fe049df8c330
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-06-17 19:42:02 +0530

    Update userService.js

--- src/user/userService.js
+++ src/user/userService.js
@@ -75,7 +75,7 @@ export async function userDashboardLoginService(details, fastify) {
       const token = await generateUserDashboardToken(email_id, fastify)
       const apiKey = encryptApiKey(user.apiKey,user.encryptionKey);
       await user.update({ token }, { where: { email_id } });
-      //fetchWithAuth(`${process.env.url}/user/dashboard/registerToken`,'POST',email_id,password,token)
+      fetchWithAuth(`${process.env.url}/user/dashboard/registerToken`,'POST',email_id,password,token)
       return { token, apiKey }
     }
     return 'Invalid email or password' 
@@ -93,7 +93,7 @@ export async function registerUserToken(details, fastify) {
       //const token = await generateUserDashboardToken(email_id, fastify)
       const apiKey = encryptApiKey(user.apiKey,user.encryptionKey);
       await user.update({ token:details.token }, { where: { email_id } });
-      fetchWithAuth(`${process.env.url}/user/dashboard/login`,'POST',email_id,password)
+      //fetchWithAuth(`${process.env.url}/user/dashboard/login`,'POST',email_id,password)
       return { token, apiKey }
     }
     return 'Invalid email or password' 
